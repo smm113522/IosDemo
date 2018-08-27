572efe812af0a533444a2759a3f0ae4a713d792b
客观开发者 <shinaimeng@126.com>
2018-08-27 09:27:42 +0800

    swift 语法学习 登录界面和功能模拟学习 tomast leading 学习

--- sexDemo/demoapp1/demoapp1/controller/LoginViewController.swift
+++ sexDemo/demoapp1/demoapp1/controller/LoginViewController.swift
@@ -13,11 +13,31 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var namefield: UITextField!
     @IBOutlet weak var psdfield: UITextField!
     
+    @IBOutlet weak var loginBtn: UIButton!
+    var HUD : MBProgressHUD?
+    
     @IBAction func loginOnclcked(_ sender: Any) {
-        
-        
-        self.performSegue(withIdentifier: "toindex", sender: nil)
+        if namefield.text != "smm" {
+            showTomast(txt: "用户名不正确")
+            return
+        }
+        if psdfield.text != "123"{
+            showTomast(txt: "密码不正确")
+            return
+        }
+        showLoading()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+//            self.showTomast(txt: "登录成功")
+            self.performSegue(withIdentifier: "toindex", sender: nil)
+        }
+       
     }
+    
+    
+    @IBAction func onChanged(_ sender: Any) {
+        loginBtn.isEnabled = (!(namefield.text?.isEmpty)! && !(psdfield.text?.isEmpty)!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         psdfield.isSecureTextEntry = true
@@ -28,8 +48,23 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func showTomast(txt:String)  {
+        HUD = MBProgressHUD(view: self.view)
+        self.view.addSubview(HUD!)
+        HUD!.label.text = txt
+        HUD!.mode = MBProgressHUDMode.text
+        HUD!.show(animated: true)
+        HUD!.hide(animated:true, afterDelay: 1)
+    }
     
-
+    func showLoading()  {
+        HUD = MBProgressHUD(view: self.view)
+        self.view.addSubview(HUD!)
+        HUD!.label.text = "登录中..."
+        //        HUD!.mode = MBProgressHUDMode.text
+        HUD!.show(animated: true)
+        HUD!.hide(animated:true, afterDelay: 3)
+    }
     
     // MARK: - Navigation
 
