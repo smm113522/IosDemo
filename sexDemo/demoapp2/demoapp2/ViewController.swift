8759ba9763ebd53409a892085a7d99295ffb1033
客观开发者 <shinaimeng@126.com>
2018-08-28 09:23:26 +0800

    效果图 添加 添加第二个demo app

---
+++ sexDemo/demoapp2/demoapp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoapp2
+//
+//  Created by shinaimeng on 2018/8/28.
+//  Copyright © 2018年 shinaimeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
